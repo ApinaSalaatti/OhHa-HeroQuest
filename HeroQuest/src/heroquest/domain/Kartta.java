57f5f928b09105f7d2967843beeb695cbc08d247
Mikko E Merioksa <merioksa@dhp-29.cs.helsinki.fi>
2012-11-01 16:36:07 +0200

    jotain

--- HeroQuest/src/heroquest/domain/Kartta.java
+++ HeroQuest/src/heroquest/domain/Kartta.java
@@ -5,22 +5,34 @@
 package heroquest.domain;
 
 import java.util.ArrayList;
-
+import java.util.Random;
 /**
  *
  * @author merioksa
  */
 public class Kartta {
     private ArrayList<Karttapala> palat;
+    private Random random;
     
     public Kartta(int koko) {
+        random = new Random();
         palat = new ArrayList<>();
         luoPalat(koko);
     }
     
     public void luoPalat(int koko) {
         for(int i = 0; i < koko; i++) {
-            palat.add(new Karttapala());
+            Karttapala uusi = new Karttapala();
+            lisaaKarttaan(uusi);
+            palat.add(uusi);
+        }
+    }
+    
+    public void lisaaKarttaan(Karttapala uusi) {
+        if(!palat.isEmpty()) {
+            Karttapala nykyinen = palat.get(0);
+            int suunta = random.nextInt(4);
+            
         }
     }
     
